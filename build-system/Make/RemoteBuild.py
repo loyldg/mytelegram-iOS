4e9f09e2a17130caafdda5d7bc1442d58a8e9b7f
Isaac <>
2025-11-06 00:06:45 +0400

    Debug

--- build-system/Make/RemoteBuild.py
+++ build-system/Make/RemoteBuild.py
@@ -26,7 +26,7 @@ def import_module_from_file(module_name, file_path):
 
 def session_scp_upload(session, source_path, destination_path):
     print('Using ssh private key path {}'.format(session.private_key_path))
-    scp_command = 'scp -i {privateKeyPath} -o BatchMode=yes -o LogLevel=ERROR -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -pr {source_path} containerhost@"{ipAddress}":{destination_path}'.format(
+    scp_command = 'scp -i {privateKeyPath} -o LogLevel=ERROR -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -pr {source_path} containerhost@"{ipAddress}":{destination_path}'.format(
         privateKeyPath=session.private_key_path,
         ipAddress=session.ip_address,
         source_path=shlex.quote(source_path),
